2dcdb4f1b8d73e82c669ad28c181e11a9b8712a5
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2016-12-31 04:11:30 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# conan-curl-cpp
+
+[Conan.io](https://conan.io) package for CURL C++ Client library
+
+## Build packages
+
+Download conan client from [Conan.io](https://conan.io) and run:
+
+    $ python build.py
+    
+## Upload packages to server
+
+    $ conan upload curl-cpp/latest@eliaskousk/stable --all
+    
+## Reuse the packages
+
+### Basic setup
+
+    $ conan install curl-cpp/latest@eliaskousk/stable/
+    
+### Project setup
+
+If you handle multiple dependencies in your project is better to add a *conanfile.txt*
+    
+    [requires]
+    curl-cpp/latest@latest/stable
+
+    [options]
+
+    [generators]
+    txt
+    cmake
+
+Complete the installation of requirements for your project running:
+
+    conan install . 
+
+Project setup installs the library (and all his dependencies) and generates the files
+*conanbuildinfo.txt* and *conanbuildinfo.cmake* with all the paths and variables that
+you need to link with your dependencies.
---
+++ conanfile.py
@@ -0,0 +1,52 @@
+from conans import ConanFile
+import os
+from conans.tools import download
+from conans.tools import unzip
+from conans import CMake
+
+class CurlCppConan(ConanFile):
+    name = "curl-cpp"
+    version = "latest"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {}
+    default_options = []
+    url="http://github.com/eliaskousk/conan-curl-cpp"
+    license="https://opensource.org/licenses/MIT"
+    exports= "CMakeLists.txt", "change_dylib_names.sh"
+    folder_name = "curl-cpp-%s" % version
+
+    def source(self):
+
+        self.run("git clone https://github.com/JosephP91/curlcpp.git %s" % self.folder_name)
+        self.run("cd %s && git checkout master" % self.folder_name)
+
+    def build(self):
+        cmake = CMake(self.settings)
+        if self.settings.os == "Windows":
+            self.run("IF not exist _build mkdir _build")
+        else:
+            self.run("mkdir _build")
+        cd_build = "cd _build"
+        self.run("%s && cmake -DBUILD_SHARED_LIBS=ON .. %s" % (cd_build, cmake.command_line))
+        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
+
+    def package(self):
+
+        if self.settings.os == "Macos":
+            self.run("bash ./change_dylib_names.sh")
+
+        # Copying headers
+        self.copy(pattern="*.h", dst="include", src="./%s/include" % self.folder_name, keep_path=False)
+
+
+        libdir = "./_build/%s/src" % self.folder_name
+        # Copying static and dynamic libs
+        self.copy(pattern="*.a", dst="lib", src=libdir, keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src=libdir, keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=libdir, keep_path=False)
+        self.copy(pattern="*.dylib*", dst="lib", src=libdir, keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", src=libdir, keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ['curlcpp curl']
