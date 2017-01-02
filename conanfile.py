51d95340dad96febd0f6a0764524529fa717a29e
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-01-02 20:29:49 +0200

    Build either static or shared
    And other small improvements

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class CurlCppConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
-    default_options = "shared=True"
+    default_options = "shared=False"
     requires = "libcurl/7.52.1@eliaskousk/stable"
     url="http://github.com/eliaskousk/conan-curl-cpp"
     license="https://opensource.org/licenses/MIT"
@@ -18,7 +18,6 @@ class CurlCppConan(ConanFile):
     folder_name = "curl-cpp-%s" % version
 
     def source(self):
-
         self.run("git clone https://github.com/JosephP91/curlcpp.git %s" % self.folder_name)
         self.run("cd %s && git checkout master" % self.folder_name)
 
@@ -29,22 +28,24 @@ class CurlCppConan(ConanFile):
         else:
             self.run("mkdir _build")
         cd_build = "cd _build"
-        self.run("%s && cmake -DBUILD_SHARED_LIBS=ON .. %s" % (cd_build, cmake.command_line))
+        shared = "-DBUILD_SHARED_LIBS=1" if self.options.shared else ""
+        self.run("%s && cmake .. %s %s" % (cd_build, cmake.command_line, shared))
         self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
 
     def package(self):
-
         if self.settings.os == "Macos":
             self.run("bash ./change_dylib_names.sh")
 
         # Copying headers
         self.copy(pattern="*.h", dst="include", src="./%s/include" % self.folder_name, keep_path=False)
 
-
-        libdir = "./_build/%s/src" % self.folder_name
-        # Copying static and dynamic libs
+        # Copying static libs
+        libdir = "./_build/lib"
         self.copy(pattern="*.a", dst="lib", src=libdir, keep_path=False)
         self.copy(pattern="*.lib", dst="lib", src=libdir, keep_path=False)
+
+        # Copying dynamic libs
+        libdir = "./_build/%s/src" % self.folder_name
         self.copy(pattern="*.so*", dst="lib", src=libdir, keep_path=False)
         self.copy(pattern="*.dylib*", dst="lib", src=libdir, keep_path=False)
         self.copy(pattern="*.dll", dst="bin", src=libdir, keep_path=False)
