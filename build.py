2304f12bbd5a89ffd2104a29ce6568e1041252aa
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-01-04 08:12:59 +0200

    Remove x86 builds

--- build.py
+++ build.py
@@ -3,6 +3,14 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="curl-cpp:shared", pure_c=False)
-    builder.run()
 
+    builder.add_common_builds(shared_option_name="curl-cpp:shared", pure_c=False)
+
+    accepted_builds = []
+    if platform.system() == "Linux" or platform.system() == "Darwin":
+        for build in builder.builds:
+            if build[0]["arch"] != "x86":
+                accepted_builds.append([build[0], build[1]])
+        builder.builds = accepted_builds
+
+    builder.run()
