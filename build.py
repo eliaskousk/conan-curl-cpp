8cb888bb39e6879a5472d19a0d5e37324a2017e9
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-01-06 11:27:07 +0200

    Remove OSx builds and restore x86 in Linux

--- build.py
+++ build.py
@@ -4,14 +4,5 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-
     builder.add_common_builds(shared_option_name="curl-cpp:shared", pure_c=False)
-
-    accepted_builds = []
-    if platform.system() == "Linux" or platform.system() == "Darwin":
-        for build in builder.builds:
-            if build[0]["arch"] != "x86":
-                accepted_builds.append([build[0], build[1]])
-        builder.builds = accepted_builds
-
     builder.run()
