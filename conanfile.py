2cba2511599f05a22d4535e9f814b6497d6446dd
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-01-17 18:12:30 +0200

    Improve requirement config and build in parallel

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ import os
 from conans.tools import download
 from conans.tools import unzip
 from conans.tools import replace_in_file
+from conans.tools import cpu_count
 from conans import CMake
 
 class CurlCppConan(ConanFile):
@@ -12,11 +13,14 @@ class CurlCppConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fpic": [True, False]}
     default_options = "shared=False", "fpic=True"
-    requires = "libcurl/7.52.1@eliaskousk/stable"
     url="http://github.com/eliaskousk/conan-curl-cpp"
     license="https://opensource.org/licenses/MIT"
     exports= "CMakeLists.txt", "cmake/detect.cpp11.cmake", "change_dylib_names.sh"
     folder_name = "curl-cpp-%s" % version
+
+    def config_options(self):
+        self.requires.add("libcurl/7.52.1@eliaskousk/stable", private=False)
+        self.options['libcurl'].shared = self.options.shared
 
     def source(self):
         self.run("git clone https://github.com/JosephP91/curlcpp.git %s" % self.folder_name)
@@ -47,7 +51,7 @@ endif()
         replace_in_file(os.path.join(self.folder_name, "src", "CMakeLists.txt"), text_to_replace, replaced_text)
 
         self.run("%s && cmake .. %s %s %s" % (cd_build, cmake.command_line, shared, fpic))
-        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
+        self.run("%s && cmake --build . %s -- -j%s" % (cd_build, cmake.build_config, cpu_count()))
 
     def package(self):
         if self.settings.os == "Macos":
